792130a03900ba9ef243d6aa29865814b146b904
KanadeShiina <820233016@qq.com>
2020-03-12 20:48:47 +0800

    some ensurefs

--- login/forms.py
+++ login/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from . import models
+from django.db.models import Q
+from django.core.exceptions import ValidationError
 
 class UserForm(forms.Form):
     username = forms.CharField(label='学号/工号', max_length=32, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -7,6 +9,20 @@ class UserForm(forms.Form):
 
 
 class InputForm(forms.ModelForm):
+    def clean(self):
+        cd = self.cleaned_data
+        pscj = cd.get("pscj")
+        zpcj = cd.get("zpcj")
+        kscj = cd.get("kscj")
+
+        if pscj < 0 or pscj > 100:
+            raise ValidationError("Invalid data")
+        if kscj < 0 or kscj > 100:
+            raise ValidationError("Invalid data")
+        if zpcj < 0 or zpcj > 100:
+            raise ValidationError("Invalid data")
+        return cd
+
     class Meta:
         model = models.Event
         exclude = ('id',)
